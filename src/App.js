e75bb8f35a4a6f6e5330d102adbe11323b9449c7
Angela Yuan <angelayuan@Angelas-MacBook-Air-3.local>
2023-01-09 00:01:51 -0500

    testing committing to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './routes/login/login.component';
+import ToDoListHome from './routes/ToDoListHome/to-do-list-home.component';
+
+import {React} from 'react';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Root">
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path = "/" element ={<Login />} />
+            <Route path = "/home" element ={<ToDoListHome />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,79 @@
+/*  interface file to connect to our Firebase Authentication and 
+Firestore services*/
+
+import { initializeApp } from "firebase/app";
+
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+    createUserWithEmailAndPassword
+} from "firebase/auth";
+import {
+    getFirestore,
+    getDoc,
+    doc,
+    updateDoc,
+    setDoc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+};
+
+/*initialzing firebase*/
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+/*database*/
+export const db = getFirestore(app);
+
+export const logInWithEmailAndPassword = async (email, password) => {
+    await signInWithEmailAndPassword(auth, email, password);
+}
+
+export const registerWithEmailAndPassword = async (email, password) => {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+
+    await setDoc(doc(db, "to-do", email), {
+        items: [],
+    });
+
+};
+
+/* returns to-do list of the user signed in*/
+export const getMyToDoList = async (email) => {
+    try {
+        const docSnap = await getDoc(doc(db, "to-do", email));
+        return docSnap.data().items;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+export const addToDoItem = async (currentItems, email, newItem) => {
+    await updateDoc(doc(db, "to-do", email), {
+        items: [...currentItems, newItem],
+    }); 
+}
+
+export const deleteToDoItem = async (currentItems, email, deleteItem) => {
+    var newItems = currentItems;
+    
+    const index = newItems.indexOf(deleteItem);
+
+    /*removes the item at that index that you wanna delete, just that one*/
+    if (index > -1) {
+        newItems.splice(index, 1);
+    }
+
+    await updateDoc(doc(db, "to-do", email), {
+        items: [...newItems],
+    });
+}
